6ffda7b01dd8b3949cc45c8aab13f0c9b9aba0bb
Felipe de Souza Paiva <feehpaiva1992@gmail.com>
2014-07-10 20:59:45 -0300

    Initial Commit

---
+++ src/controler/ServletController.java
@@ -0,0 +1,37 @@
+package controler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import util.RequisicaoWebService;
+import util.WriterData;
+
+
+@WebServlet("/ServletController")
+public class ServletController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public ServletController() {super();}
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String origem = request.getParameter("origem");
+		String destino = request.getParameter("destino");
+		
+		RequisicaoWebService requisicaoWebService = new RequisicaoWebService();
+		JSONObject jsonObject = requisicaoWebService.request(origem, destino);
+
+		WriterData.writeJsonObject(jsonObject, response);
+		
+	}
+
+}
